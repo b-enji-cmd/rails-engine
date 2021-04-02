933f4a1f7fc2cac10b7de7e6c758d772ebe2c48e
Ben Fulton <benjamin.fulton99@gmail.com>
2021-04-02 14:24:55 -0600

    Update structure for postman testing

--- app/controllers/api/v1/merchants/merchant_items_controller.rb
+++ app/controllers/api/v1/merchants/merchant_items_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Merchants::MerchantItemsController < ApplicationController
 
 	#GET /api/vi/merchants/:id/items
 	def index
-		json_response(ItemSerializer.new(MerchantFacade.find_merchant_items(params[:id])))
+		json_response(ItemSerializer.new(Merchant.find(params[:id]).items))
 	end
 end
